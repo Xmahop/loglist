384f046844679533c91a2329eaf27b4d67dabeb6
xmahop <xmahop@MacBook-Xmahop.local>
2019-01-31 20:15:31 +0300

    new styles

--- loglist/loglists/forms.py
+++ loglist/loglists/forms.py
@@ -5,7 +5,7 @@ from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from django.urls import reverse
 
 class PostForm(forms.ModelForm):
-    switch_ip = forms.CharField(label='IP адрес',widget=forms.TextInput(attrs={'placeholder':'IP адрес',}))
+    switch_ip = forms.CharField(label='IP адрес',widget=forms.TextInput(attrs={'placeholder':'IP адрес'}))
     text = forms.CharField(label='описание',widget=forms.TextInput(attrs={'placeholder':'Описание'}))
     class Meta:
         model = Post
